55761e78e5a7d62e07cc07060955ef895ea10a0b
Dustin <165263887+dustin314@users.noreply.github.com>
2024-12-14 02:29:09 +0700

    update

--- Python/createheart.py
+++ Python/createheart.py
@@ -10,13 +10,10 @@ def corazon(k):
     return 15 * math.sin(k) ** 3
 
 def corazon1(k):
-    return 12 * math.cos(k) - 5 * 
-                 math.cos(2*k) - 2 * 
-                 math.cos(3*k) - 
-                 math.cos(4*k) 
+    return 12 * math.cos(k) - 5 * math.cos(2*k) - 2 * math.cos(3*k) - math.cos(4*k) 
 
 for i in range(600): 
-    t.goto(corazon(i) * 18, coraaon1(i) * 18)
+    t.goto(corazon(i) * 18, corazon(i) * 18)
     for j in range(5): 
         t.color("red")
         
